1269d3725cc1cb1b673c0bfd3ac0474161bb1df6
Bárdó <rbardo@gmail.com>
2023-04-17 17:13:31 +0200

    Zsolt verziója

--- 1/ex1/Dog.cs
+++ 1/ex1/Dog.cs
@@ -6,25 +6,29 @@ namespace MyApp
         public String fajta { get; set; }
         public int Eletero { get; set; }
         public int Harciero { get; set; }
+        public bool HarcolniKepes;
+        public int UjEro;
         public Dog(String name, String color, int Eletero, int Harciero) : base(name, color)
         {
             this.Eletero = Eletero;
             this.Harciero = Harciero;
         }
-
-        public bool Harcol(Dog masik)
+        public bool Harcol(int ActualEletero)
         {
-            return true;
+            HarcolniKepes=true;
+            if (ActualEletero<=0) HarcolniKepes=false;
+            return HarcolniKepes;
         }
-
-        public void sebzes(int ero)
+        public int Sebzes(int Ero, int HarciEro)
         {
-
+            Random ValosHarciEro=new Random();            
+            UjEro=Ero-ValosHarciEro.Next(1,HarciEro);
+            return UjEro;
         }
-
         public new void speak()
         {
             Console.WriteLine("Vau vau a nevem: " + name);
+            Console.WriteLine($"{Eletero},{Harciero}");
         }
     }
 }
--- 1/ex1/Program.cs
+++ 1/ex1/Program.cs
@@ -8,22 +8,28 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Dog dog1 = new Dog("Cézár", "Barna", 300, 40);
             Dog dog2 = new Dog("Roti", "Barna", 250, 50);
-            Animal cat = new Animal("Cézár", "Barna");
-            Animal allat;
-            allat = dog1;
-            Console.WriteLine(dog1.GetHashCode());
-            Console.WriteLine($"{cat}");
-            allat.name = "Bodri";
-            dog1.speak();
-            // cat.speak("Miau");
-            // cat.speak(4);
-            //dog.Harcol();
-
-            while (dog1.Harcol(dog2))
+            int iKorokSzama=0;
+            int ActualEletero1=dog1.Eletero;
+            int ActualEletero2=dog2.Eletero;
+            Console.WriteLine(ActualEletero1);
+            while (dog1.Harcol(ActualEletero1) && dog2.Harcol(ActualEletero2))
             {
-                /// körök szám és a támadások paramétere
+                iKorokSzama++;
+                ActualEletero1=dog1.Sebzes(ActualEletero1,dog2.Harciero);
+                ActualEletero2=dog2.Sebzes(ActualEletero2,dog1.Harciero);
+                dog1.Harcol(ActualEletero1);
+                Console.WriteLine($"{iKorokSzama}. kör. Az állás: {dog1.name} {ActualEletero1} {dog2.name} {ActualEletero2}.");
             }
-            // ki a győztes
+            if (dog1.Harcol(ActualEletero1)==false && dog2.Harcol(ActualEletero2)==true)
+                Console.WriteLine($"A harcot a {dog2.name} nevű kutya nyerte {iKorokSzama} menetben. Életereje {ActualEletero2}. A legyőzött kutyáé {ActualEletero1}.");
+            if (dog2.Harcol(ActualEletero2)==false && dog1.Harcol(ActualEletero1)==true)
+                Console.WriteLine($"A harcot a {dog1.name} nevű kutya nyerte {iKorokSzama} menetben. Életereje {ActualEletero1}. A legyőzött kutyáé {ActualEletero2}.");
+            if (dog2.Harcol(ActualEletero2)==false && dog1.Harcol(ActualEletero1)==false){
+                if (ActualEletero1>ActualEletero2)
+                    Console.WriteLine($"A harcot a {dog1.name} nevű kutya nyerte {iKorokSzama} menetben. Életereje {ActualEletero1}. A legyőzött kutyáé {ActualEletero2}.");
+                if (ActualEletero2>ActualEletero1)
+                    Console.WriteLine($"A harcot a {dog2.name} nevű kutya nyerte {iKorokSzama} menetben. Életereje {ActualEletero2}. A legyőzött kutyáé {ActualEletero1}.");
+            }
         }
     }
 }
